c32f490d2ef0699ec2f902cd1e637f541ab80b24
EdenCoder <alex@adlist.co.nz>
2018-11-01 15:41:29 +1000

    initial commit

---
+++ bundles/email/controllers/email.js
@@ -0,0 +1,30 @@
+
+// Require dependencies
+const uuid       = require('uuid');
+const config     = require('config');
+const sitemap    = require('sitemap');
+const Controller = require('controller');
+
+/**
+ * Build Email controller
+ *
+ * @priority 90
+ */
+class EmailController extends Controller {
+
+  /**
+   * Construct Email controller class
+   */
+  constructor () {
+    // Run super eden
+    super();
+
+  }
+}
+
+/**
+ * Export Email controller
+ *
+ * @type {EmailController}
+ */
+exports = module.exports = EmailController;
